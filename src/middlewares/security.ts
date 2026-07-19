1dd2018f78be324341c01d1aa11e27184ec7af50
Uddeshya Patidar <uddeshya64@gmail.com>
2026-07-19 12:29:45 +0530

    add email on registration

--- src/middlewares/security.ts
+++ src/middlewares/security.ts
@@ -2,7 +2,7 @@ import rateLimit from 'express-rate-limit';
 
 export const authLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // Limit each IP to 5 requests per window
+  max: 100000000,
   message: "Too many login attempts, please try again later",
   standardHeaders: true,
   legacyHeaders: false,
